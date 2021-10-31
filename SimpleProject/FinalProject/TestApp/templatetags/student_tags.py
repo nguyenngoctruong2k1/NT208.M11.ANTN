fa2360b9f5467a2149d58f76e0a8f1606d9d4a78
Nguyễn Ngọc Trưởng <2k1.nguyenngoctruong@gmail.com>
2021-10-31 20:25:40 +0700

    update

--- SimpleProject/FinalProject/TestApp/templatetags/student_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from TestApp.models import Student
-
-register = template.library()
-
-@register.simple_tag()
-def get_students():
-    return Student.objects.all()
--- SimpleProject/FinalProject/TestApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from TestApp import views
-
-urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('create-new-student/', views.create_new_student_view, name='create_new_student_view'),
-    path('create-new-student/ok/', views.new_student_form_ok, name='new_student_form_ok'),
-]
