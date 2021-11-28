54f8448cb45c695c1d3b7ae0cb16dbf614a1c311
Truong Nguyen <2k1.nguyenngoctruong@gmail.com>
2021-11-28 12:19:18 +0700

    updatedjango_settings

--- FINALPROJECT/settings.py
+++ FINALPROJECT/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ln6#5=u=x#)+x^z=8q8#&1lhi*+nzyy6q7_wjjfuy1kd2k%kb%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 # ALLOWED_HOSTS = []
--- FINALPROJECT/urls.py
+++ FINALPROJECT/urls.py
@@ -21,11 +21,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myproject.urls'))
+    path('', include('myproject.urls')),
+    # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 
-""" if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) """
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 """ handler404 = 'myproject.views.error'
 handler500 = 'myproject.views.error' """
