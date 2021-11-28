b8a77831f1572411e0ec8784b4aba89290b4a948
Truong Nguyen <2k1.nguyenngoctruong@gmail.com>
2021-11-28 09:53:22 +0700

    updatedjango_static

--- settings.py
+++ settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR/"static" 
+# MEDIA_URL  = '/media/'
+# MEDIA_ROOT = BASE_DIR/"media" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
